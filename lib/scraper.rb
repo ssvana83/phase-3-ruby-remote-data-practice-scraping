b7d9a96331bbe6f7c98662a62391302c8f246490
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-28 16:08:47 -0400

    commit tree a63aa398851686c84fc01c7d739c7111128389e3

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+# doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".heading-40-semibold").text.strip
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
+
+puts doc.css(".heading-40-semibold")
